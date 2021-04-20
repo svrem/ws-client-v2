4cb5e1d673c9cf513c80045a4dabf973bc1ccdc5
svrem125 <svrem@icloud.com>
2021-04-20 16:14:45 +0200

    changed the sendform to a form so you can press enter

--- renderer/components/sendform.js
+++ renderer/components/sendform.js
@@ -5,28 +5,29 @@ import { useRouter } from "next/router";
 const sendform = ({ setMessages, messages, sendMessage }) => {
   const router = useRouter();
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sendMessage(document.getElementById("message").value);
+    document.getElementById("message").value = "";
+  };
+
   return (
     //
     // }}
-    <div className={styles.sendForm}>
+    <form className={styles.sendForm} onSubmit={handleSubmit}>
       <button
         onClick={() => router.push("/home")}
         className={styles.Disconnect}
+        type="button"
       >
         Disconnect
       </button>
 
       <input id="message" placeholder="Message" className={styles.input} />
-      <button
-        onClick={() => {
-          sendMessage(document.getElementById("message").value);
-          document.getElementById("message").value = "";
-        }}
-        className={styles.Send}
-      >
+      <button className={styles.Send} type="submit">
         Send
       </button>
-    </div>
+    </form>
   );
 };
 
